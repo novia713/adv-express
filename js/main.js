de0e5610091d1ea3623a2bc136e9003131c7cd79
Leandro Vázquez Cervantes <leandro.vazquez@devtopia.coop>
2015-03-21 18:31:29 +0100

    first shot

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    $('#content').html("<img src='images/spinner.gif' width='50px' height='50px'/>");
+    var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=20&q=http://feeds.feedburner.com/AscoDeVida';
+    
+    $.ajax({
+				type:'GET',
+				dataType:'jsonp',
+				jsonp:'callback',
+        data: {format: "json"},
+				url: url,
+				success:function(data) {
+          var printa = function(e){
+            $('#content').append("<div class='item fit'><dl><dt class='title round info label fit'>" + e.title.replace("y su ADV","") + "</dt><dd class='fit txt'>" + e.content.split("<br><br>")[0] + "</dd></dl></div><br/>");
+
+          }
+
+          $('#content').html("");
+          data.responseData.feed.entries.map(printa);
+				},
+				error:function() {
+					alert("Sorry, I can't get the feed");
+				}
+			});
+
+});
